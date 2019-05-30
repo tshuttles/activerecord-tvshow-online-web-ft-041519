717e8cdf8f79271e53ceb559265638337aecba1c
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-30 04:44:31 +0000

    Done.

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -3,7 +3,7 @@ class CreateShows < ActiveRecord::Migration[5.2]
   def change 
     create_table :shows do |t|
       t.string :name 
-      t.string :network  
+      t.string :network
       t.string :day 
       t.integer :rating 
     end 
