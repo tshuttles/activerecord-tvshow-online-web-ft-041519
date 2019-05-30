c70a64f4793aa041a6dc64e66cbf4da800710b7c
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-30 04:44:10 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -21,7 +21,7 @@ class Show < ActiveRecord::Base
   end 
   
   def self.popular_shows 
-    self.where("rating > 5", params[:rating])
+    self.where("rating > 5")
   end 
   
   def self.shows_by_alphabetical_order 
